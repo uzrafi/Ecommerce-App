bbcbd6e5956b4541ce5a652d47ce87a29281cdb1
uzrafi <uzairrafi159@gmail.com>
2023-11-04 21:39:39 -0400

    Initial setup commit

---
+++ ember-cli-build.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+module.exports = function (defaults) {
+  const app = new EmberApp(defaults, {
+    // Add options here
+    sassOptions: {
+      extension: "scss",
+    }
+  });
+
+  return app.toTree();
+};
