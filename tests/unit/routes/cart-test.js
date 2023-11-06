e60449d748e9e4f9b9ced4626e2617d4a2e1c9ed
uzrafi <uzairrafi159@gmail.com>
2023-11-05 20:52:48 -0500

    Added components for products

---
+++ tests/unit/routes/cart-test.js
@@ -0,0 +1,11 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ecommerce-app/tests/helpers';
+
+module('Unit | Route | cart', function (hooks) {
+  setupTest(hooks);
+
+  test('it exists', function (assert) {
+    let route = this.owner.lookup('route:cart');
+    assert.ok(route);
+  });
+});
